f098226e3d6ea4cd5f14d45a474c7fed7a42fff7
giocode <giocordova@gmail.com>
2020-11-16 12:21:22 -0500

    initial commit and setup. still need to complete setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Beef');
+INSERT INTO burgers (burger_name) VALUES ('Bison');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Venison', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Avocado', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
